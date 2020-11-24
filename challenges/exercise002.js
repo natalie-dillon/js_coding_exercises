b4e068c831127dd9c96b12dee5c929dc95bdac68
Natalie <natalie@sca.design>
2020-11-24 22:31:31 +0000

    Completed challenge exercise002.js

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,33 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city.toLowerCase() === "manchester"; 
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+ return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let numberOfSheep = 0;
+  arr.forEach((animal) => {
+    if (animal.toLowerCase() === "sheep")
+    {
+      numberOfSheep++;
+    }
+  });
+  return numberOfSheep;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.address.city.toLowerCase() === "manchester" && person.address.postCode.charAt(0).toUpperCase() === "M";
 }
 
 module.exports = {
