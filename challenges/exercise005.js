140450213e0d135386866c8fbacdf8f1ae7e847f
Natalie <natalie@sca.design>
2020-11-26 13:41:08 +0000

    Solve exercise 5

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,38 +1,90 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  let ind = nums.findIndex(num => num === n);
+  if (ind === -1 || ind === nums.length - 1){
+    return null;
+  }
+  return nums[ind + 1];
 };
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  const frequencies = {
+    1: 0,
+    0: 0
+  };
+  for (let i=0; i< str.length; i++){
+    let chr = str[i];
+    if (frequencies[chr] !== undefined)
+    {
+      frequencies[chr] += 1;
+    }    
+  }
+  return frequencies;
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  let str = n.toString();
+  let reverseStr = "";
+  for (let i=str.length - 1; i >= 0; i--){
+    if (str[i] != 0 || reverseStr.length > 0){
+      reverseStr += str[i];
+    }
+  }
+  return Number(reverseStr);
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  let total = 0;
+  for (let i=0; i< arrs.length; i++){
+    for (let j=0; j< arrs[i].length; j++)
+    {
+      total += arrs[i][j];
+    }
+  }
+  return total;
 };
 
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  if (arr.length < 2){
+    return arr;
+  }
+  let temp = arr[0];
+  arr[0] = arr[arr.length - 1];
+  arr[arr.length - 1] = temp;
+  return arr;
 };
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  for (const needle in haystack){
+    let val = haystack[needle].toString().toLowerCase();
+    if (val.includes(searchTerm.toLowerCase()))
+    {
+      return true;
+    }
+  }
+  return false;
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  const frequencies = {};
+  const words = str.split(" ");
+  words.forEach(word => {
+      const cleanWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+      if (frequencies[cleanWord] === undefined){
+        frequencies[cleanWord] = 1;
+      } else {
+        frequencies[cleanWord] += 1;
+      }
+  });
+  return frequencies;
 };
 
 module.exports = {
